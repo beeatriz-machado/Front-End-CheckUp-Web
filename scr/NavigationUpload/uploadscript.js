a24f70177e2be81c1fffd80cd6d4782ebc20ed34
Beatriz Machado <84201596+beeatriz-machado@users.noreply.github.com>
2024-10-28 23:50:48 -0300

    Primeiras telas

---
+++ scr/NavigationUpload/uploadscript.js
@@ -0,0 +1,23 @@
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+// }
+
+// function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+// }
+
+// Função para adicionar mais botões dinamicamente
+function addNavButton() {
+    var btnName = document.getElementById("btnName").value;
+
+    if (btnName.trim() !== "") {
+        var newButton = document.createElement("button"); // Cria o novo botão (elemento <a>)
+        newButton.href = "#";
+        newButton.textContent = btnName;
+        newButton.classList.add("nav-btn"); // Adiciona a classe 'nav-btn' ao botão
+
+        document.getElementById("mySidenav").appendChild(newButton); // Adiciona o novo botão à barra lateral
+    } else {
+        alert("Please enter a valid button name.");
+    }
+}
