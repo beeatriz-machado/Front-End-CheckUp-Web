00868db0ebb07a0bf549d135dbbe4d6098b3613f
Beatriz Machado <84201596+beeatriz-machado@users.noreply.github.com>
2024-11-10 16:20:21 -0300

    Navegar entre paginas

---
+++ scr/Otimizar/Adicionarpagina.js
@@ -0,0 +1,3 @@
+function alterarPagina(pagina) {
+    window.location.href = pagina; // Substitua pelo nome do arquivo HTML
+}
--- scr/Otimizar/CarregaConteudoHtml.js
+++ scr/Otimizar/CarregaConteudoHtml.js
@@ -8,6 +8,3 @@
     }
 
 
-function alterarPagina(pagina) {
-    window.location.href = pagina; // Substitua pelo nome do arquivo HTML
-}
